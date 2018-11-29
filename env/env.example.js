44a61085e06d1b9e9954aaa3ed0463de564667f2
Dylan <dylanmccarthy@Dylans-MacBook-Pro.local>
2018-11-29 14:19:35 -0800

    trying to connect to db-instance

--- env/env.example.js
+++
@@ -1,9 +0,0 @@
-const env = {
-  database: 'yourdatabase',
-  dbHost: 'your database login credential',
-  dbPassword: 'your database login password',
-  PORT: 'your port number',
-  HOSTNAME: 'localhost',
-};
-
-module.exports = { env };
